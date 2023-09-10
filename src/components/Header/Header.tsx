d33041f0a0d241b6a5032080f8d24a06ba96aee4
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-09-10 16:15:16 +0700

    test

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+export default function Header () {
+    {/* If   */}
+    return (
+        <>
+            <div style={{float : "left", display: "flex"}}>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Store</a>
+                    <a href="">Support</a>
+                    <a href="">Documentation</a>
+                </nav>
+                <button>
+                    {/* Icon goes here */}<a href="/Logout">Logout</a>
+                </button>   
+            </div>
+        </>
+    )
+
+
+
+}
