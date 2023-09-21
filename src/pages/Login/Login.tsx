ec43e68712d2b3c288162418b65abefe131ca1b9
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-09-21 23:27:39 +0700

    feat: basic login layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Box, TextField, Container, Typography } from '@mui/material'
 
 
 export const Login: React.FC = () => {
 	return (
 		<>
-			<div></div>
+			<Container maxWidth="sm">
+				<Typography my={5} component="h1">
+					Welcome to GHub!
+				</Typography>
+				<Typography my={2}  component="p">
+					Please sign-in to your account and start the adventure
+				</Typography>
+
+				<Box sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					'& .MuiTextField-root': { width: '25ch' },
+				}}>
+					<TextField id="email-input" label="Email Address" variant="outlined"/>
+					<TextField id="password-input" label="Password" variant="outlined" />
+				</Box>
+			</Container>
 		</>
 	)
 }
