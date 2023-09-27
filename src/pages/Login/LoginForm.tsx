e2256ddc1c460c74b81dcf70a399cf22c867dfda
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-09-27 17:17:50 +0700

    feat: use reactHookForm, add validation

--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -8,63 +8,100 @@ import {
 	Typography,
 } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 
 import LoginButton from './LoginButton/LoginButton'
 
 
+type LoginInput = {
+	email: string,
+	password: string,
+	remembered: boolean,
+}
+
+const EmailTextField: React.FC<{ errors: any, register: any }> = ({ errors, register }) => (
+	<TextField
+		id="email-input"
+		label="Email Address"
+		variant="outlined"
+		margin="normal"
+		type="email"
+		{...register('email', {
+			required: 'Email is Required',
+			pattern: {
+				// eslint-disable-next-line no-useless-escape
+				value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+				message: 'Not a valid email address'
+			}
+		})}
+		error={!!errors.email}
+		helperText={errors.email?.message}
+	/>
+)
+
+const PasswordTextField: React.FC<{ errors: any, register: any }> = ({ errors, register }) => (
+	<TextField
+		id="password-input"
+		label="Password"
+		variant="outlined"
+		margin="normal"
+		type="password"
+		{...register('password', {
+			required: 'Password is Required',
+			pattern: {
+				value: /^[A-Za-z](?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{5,19}$/,
+				message: 'Password must start with a letter, contain at least 1 uppercase letter, 1 lowercase letter. Length should be 6-20 characters.'
+			}
+		})}
+		error={!!errors.password}
+		helperText={errors.password?.message}
+	/>
+)
+
+const RememberMeCheckbox: React.FC<{ watch: any, register: any }> = ({ watch, register }) => (
+	<FormGroup>
+		<FormControlLabel
+			control={
+				<Checkbox
+					checked={watch('remembered')}
+					{...register('remembered')}
+				/>
+			}
+			label="Remember Me"
+		/>
+	</FormGroup>
+)
+
 const LoginForm: React.FC = () => {
-	const [isChecked, setChecked] = useState<boolean>(true)
-	const [email, setEmail] = useState<string>('')
-	const [password, setPassword] = useState<string>('')
-
-	const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setEmail(event.target.value)
+	const form = useForm<LoginInput>({
+		mode: 'onChange',
+		defaultValues: {
+			email: '',
+			password: '',
+			remembered: false,
+		}
+	})
+	const { watch, register, handleSubmit, formState, control } = form
+	const { errors } = formState
+	
+	const onSubmit = (data: LoginInput) => {
+		console.log({...data})
 	}
 
-	const handlePasswordChange = (
-		event: React.ChangeEvent<HTMLInputElement>
-	) => {
-		setPassword(event.target.value)
-	}
-
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setChecked(event.target.checked)
-	}
-
-	const handleLoginClick = () => {
-		const loginData = `Email: ${email}, Password: ${password}`
-		console.log(loginData)
-	}
-
-	return (
+	return (<>
 		<Box sx={{
 			display: 'flex',
 			flexDirection: 'column',
 		}}
 		component='form'
-		onSubmit={handleLoginClick}
+		onSubmit={handleSubmit(onSubmit)}
 		>
-			<TextField
-				id="email-input" label="Email Address" variant="outlined" margin="normal"
-				value={email} onChange={handleEmailChange}
-			/>
-			<TextField
-				id="password-input" label="Password" variant="outlined" margin="normal"
-				type="password" value={password} onChange={handlePasswordChange}
-			/>
+			<EmailTextField errors={errors} register={register} />
+			<PasswordTextField errors={errors} register={register} />
+
 			<Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-				<FormGroup>
-					<FormControlLabel
-						control={
-							<Checkbox
-								checked={isChecked}
-								onChange={handleChange}
-								inputProps={{ 'aria-label': 'controlled' }}
-							/>
-						}
-						label="Remember Me"
-					/>
-				</FormGroup>
+				<RememberMeCheckbox watch={watch} register={register} />
 
 				<Typography sx={{ 
 					color: '#DC7000', fontSize: '16px', 
@@ -76,7 +113,8 @@ const LoginForm: React.FC = () => {
 
 			<LoginButton/>
 		</Box>
-	)
+		<DevTool control={control} />
+	</>)
 }
 
 export default LoginForm
