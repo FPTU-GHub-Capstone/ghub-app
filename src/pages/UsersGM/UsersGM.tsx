230ced84281645f0ae113bee88da10646558ab57
Panh <dangphuonganh200136@gmail.com>
2023-10-11 11:33:17 +0700

    feat: fix conflict with firebase

--- src/pages/UsersGM/UsersGM.tsx
+++ src/pages/UsersGM/UsersGM.tsx
@@ -159,90 +159,88 @@ export default function UsersGM() {
 				<Card>
 					<UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
 
-					<Scrollbar>
-						<TableContainer sx={{ minWidth: 800 }}>
-							<Table>
-								<UserListHead
-									order={order}
-									orderBy={orderBy}
-									headLabel={TABLE_HEAD}
-									rowCount={USERLIST.length}
-									numSelected={selected.length}
-									onRequestSort={handleRequestSort}
-									onSelectAllClick={handleSelectAllClick}
-								/>
-								<TableBody>
-									{filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-										const { id, name, role, status, company, avatarUrl, isVerified } = row
-										const isSelectedUser = selected.indexOf(name as never) !== -1
+					<TableContainer sx={{ minWidth: 800 }}>
+						<Table>
+							<UserListHead
+								order={order}
+								orderBy={orderBy}
+								headLabel={TABLE_HEAD}
+								rowCount={USERLIST.length}
+								numSelected={selected.length}
+								onRequestSort={handleRequestSort}
+								onSelectAllClick={handleSelectAllClick}
+							/>
+							<TableBody>
+								{filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+									const { id, name, role, status, company, avatarUrl, isVerified } = row
+									const isSelectedUser = selected.indexOf(name as never) !== -1
 
-										return (
-											<TableRow hover key={id} tabIndex={-1} role="checkbox" selected={isSelectedUser}>
-												<TableCell padding="checkbox">
-													<Checkbox checked={isSelectedUser} onChange={(event) => handleClick(event, name)} />
-												</TableCell>
+									return (
+										<TableRow hover key={id} tabIndex={-1} role="checkbox" selected={isSelectedUser}>
+											<TableCell padding="checkbox">
+												<Checkbox checked={isSelectedUser} onChange={(event) => handleClick(event, name)} />
+											</TableCell>
 
-												<TableCell component="th" scope="row" padding="none">
-													<Stack direction="row" alignItems="center" spacing={2}>
-														<Avatar alt={name} src={avatarUrl} />
-														<Typography variant="subtitle2" noWrap>
-															{name}
-														</Typography>
-													</Stack>
-												</TableCell>
-
-												<TableCell align="left">{company}</TableCell>
-
-												<TableCell align="left">{role}</TableCell>
-
-												<TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
-
-												<TableCell align="left">
-													<Label color={(status === 'banned' && 'error') || 'success'}>{sentenceCase(status)}</Label>
-												</TableCell>
-
-												<TableCell align="right">
-													<IconButton size="large" color="inherit" onClick={handleOpenMenu}>
-														<Iconify icon={'eva:more-vertical-fill'} />
-													</IconButton>
-												</TableCell>
-											</TableRow>
-										)
-									})}
-									{emptyRows > 0 && (
-										<TableRow style={{ height: 53 * emptyRows }}>
-											<TableCell colSpan={6} />
-										</TableRow>
-									)}
-								</TableBody>
-
-								{isNotFound && (
-									<TableBody>
-										<TableRow>
-											<TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-												<Paper
-													sx={{
-														textAlign: 'center',
-													}}
-												>
-													<Typography variant="h6" paragraph>
-                            Not found
+											<TableCell component="th" scope="row" padding="none">
+												<Stack direction="row" alignItems="center" spacing={2}>
+													<Avatar alt={name} src={avatarUrl} />
+													<Typography variant="subtitle2" noWrap>
+														{name}
 													</Typography>
+												</Stack>
+											</TableCell>
 
-													<Typography variant="body2">
-                            No results found for &nbsp;
-														<strong>&quot;{filterName}&quot;</strong>.
-														<br /> Try checking for typos or using complete words.
-													</Typography>
-												</Paper>
+											<TableCell align="left">{company}</TableCell>
+
+											<TableCell align="left">{role}</TableCell>
+
+											<TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
+
+											<TableCell align="left">
+												<Label color={(status === 'banned' && 'error') || 'success'}>{sentenceCase(status)}</Label>
+											</TableCell>
+
+											<TableCell align="right">
+												<IconButton size="large" color="inherit" onClick={handleOpenMenu}>
+													<Iconify icon={'eva:more-vertical-fill'} />
+												</IconButton>
 											</TableCell>
 										</TableRow>
-									</TableBody>
+									)
+								})}
+								{emptyRows > 0 && (
+									<TableRow style={{ height: 53 * emptyRows }}>
+										<TableCell colSpan={6} />
+									</TableRow>
 								)}
-							</Table>
-						</TableContainer>
-						<div></div>
-					</Scrollbar>
+							</TableBody>
+
+							{isNotFound && (
+								<TableBody>
+									<TableRow>
+										<TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+											<Paper
+												sx={{
+													textAlign: 'center',
+												}}
+											>
+												<Typography variant="h6" paragraph>
+                            Not found
+												</Typography>
+
+												<Typography variant="body2">
+                            No results found for &nbsp;
+													<strong>&quot;{filterName}&quot;</strong>.
+													<br /> Try checking for typos or using complete words.
+												</Typography>
+											</Paper>
+										</TableCell>
+									</TableRow>
+								</TableBody>
+							)}
+						</Table>
+					</TableContainer>
+					<div></div>
 
 					<TablePagination
 						rowsPerPageOptions={[5, 10, 25]}
