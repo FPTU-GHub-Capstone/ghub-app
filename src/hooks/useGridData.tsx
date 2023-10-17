913b95c9e8084d0667c75fe22615b7879d8f3849
Panh <dangphuonganh200136@gmail.com>
2023-10-17 14:37:52 +0700

    feat: change code base of table

---
+++ src/hooks/useGridData.tsx
@@ -0,0 +1,75 @@
+import { GridRenderCellParams, GridColDef } from '@mui/x-data-grid'
+import { ElementType, ReactNode } from 'react'
+
+
+export interface IUseGridData<T> {
+	createColumn: (
+		field: string,
+		headerName: string | ReactNode,
+		width: number
+	) => GridColDef;
+
+	renderCell: (
+		field: string,
+		headerName: string | ReactNode,
+		width: number,
+		component: ElementType
+	) => GridColDef;
+
+	renderValueGetter: (
+		field: string,
+		headerName: string | ReactNode,
+		width: number,
+		getValue: (params: T) => string
+	) => GridColDef;
+}
+
+export const useGridData = <T extends Record<string, any>>() => {
+	const createColumn = (
+		field: string,
+		headerName: string,
+		width: number
+	): GridColDef => {
+		return {
+			field: field,
+			renderHeader: () => <strong>{headerName}</strong>,
+			width: width,
+			editable: false,
+		}
+	}
+
+	const renderCell = (
+		field: string,
+		headerName: string,
+		width: number,
+		component: ElementType
+	): GridColDef => {
+		const Component = component
+		return {
+			field: field,
+			renderHeader: () => <strong>{headerName}</strong>,
+			width: width,
+			renderCell: (params: GridRenderCellParams<any>) => (
+				<Component rowData={params?.row} />
+			),
+		}
+	}
+
+	const renderValueGetter = (
+		field: string,
+		headerName: string,
+		width: number,
+		getValue: (params: T) => string
+	): GridColDef => {
+		return {
+			field: field,
+			renderHeader: () => <strong>{headerName}</strong>,
+			width: width,
+			valueGetter: (params) => {
+				return getValue(params?.row)
+			},
+		}
+	}
+
+	return { renderCell, createColumn, renderValueGetter } as IUseGridData<T>
+}
--- src/mock/user.ts
+++ src/mock/user.ts
@@ -12,16 +12,8 @@ const users = [...Array(24)].map((_, index) => ({
 	isVerified: faker.datatype.boolean(),
 	status: sample(['active', 'banned']),
 	role: sample([
-		'Leader',
-		'Hr Manager',
-		'UI Designer',
-		'UX Designer',
-		'UI/UX Designer',
-		'Project Manager',
-		'Backend Developer',
-		'Full Stack Designer',
-		'Front End Developer',
-		'Full Stack Developer',
+		'Player',
+		'Game Manager',
 	]),
 }));
   
