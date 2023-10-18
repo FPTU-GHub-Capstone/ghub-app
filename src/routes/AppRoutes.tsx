b0b5937e10a7777f8693cb350655c9d4818e53e3
danielng <dacnguyen9101@gmail.com>
2023-10-18 22:33:51 +0700

    feat: reconfig turbo, firebase

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import { PrivateRouters, PublicRouters } from '../common'
-import { Login } from '../pages/Login'
 
 
 export const AppRoutes: React.FC = () => {
 	return (
 		<Routes>
-			<Route path="/" Component={Login} />
-			<Route path="/login" Component={Login} />
 			{PrivateRouters.map((route) => {
 				const Layout = route.layout ?? React.Fragment
 				const Component = route.component
