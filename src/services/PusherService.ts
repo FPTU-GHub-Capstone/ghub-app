9c1130beea50ab7f3605390da39345b1a018bf9d
danielng <dacnguyen9101@gmail.com>
2023-10-28 11:16:56 +0700

    feat: add app config, fix type

--- src/services/PusherService.ts
+++ src/services/PusherService.ts
@@ -1,31 +1,32 @@
 import Pusher, { Channel } from 'pusher-js';
 
+import config from '../config';
 
-const env = import.meta.env;
 
+type HandleEventCallback = (...args: unknown[]) => any
 
 class PusherService {
 	private readonly _pusher: Pusher;
 	private readonly _channel: Channel;
 
 	constructor() {
-		this._pusher = new Pusher(env.VITE_PUSHER_APP_KEY, {
-			cluster: env.VITE_PUSHER_CLUSTER,
+		this._pusher = new Pusher(config.PUSHER_APP_KEY, {
+			cluster: config.PUSHER_CLUSTER,
 		});
-		this._channel = this._pusher.subscribe(env.VITE_PUSHER_CHANNEL);
+		this._channel = this._pusher.subscribe(config.PUSHER_CHANNEL);
 	}
 
-	public bindEvents(): void {
-		this._channel.bind(env.VITE_PUSHER_BIND_INSERTED_EVENT, (data: any) => {
-			console.log('start inserted');
-			console.log(JSON.stringify(data, undefined, 2));
-			console.log('end inserted');
-		});
-		this._channel.bind(env.VITE_PUSHER_BIND_DELETED_EVENT, (data: any) => {
-			console.log('start deleted');
-			console.log(data);
-			console.log('end deleted');
-		});
+	public bindEvent(event: string, callback: HandleEventCallback, context?: any): void {
+		this._channel.bind(event, callback, context);
+		// this._channel.bind(config.VITE_PUSHER_BIND_INSERTED_EVENT, (data: any) => {
+		// 	console.log('start inserted');
+		// 	console.log(JSON.stringify(data, undefined, 2));
+		// 	console.log('end inserted');
+		// });
+	}
+
+	public unbindEvent(event: string): void {
+		this._channel.unbind(event);
 	}
 }
 
