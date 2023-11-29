2597e43fa4b2c91848191d6f9060d1b696d83f76
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-11-29 13:36:59 +0700

    feat: close Logging on newtab

--- src/Layout/LoggingLayout/Navbar/Navbar.tsx
+++ src/Layout/LoggingLayout/Navbar/Navbar.tsx
@@ -1,21 +1,38 @@
 import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
-import { ArrowBack } from '@mui/icons-material'
-import { useNavigate } from 'react-router'
+import { ArrowBack, Close } from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
 
 
 export function Navbar() {
 	const navigate = useNavigate()
 
+	const handleButtonClick = () => {
+		if (window.history.length <= 1 ) {
+			window.close()
+		} else {
+			navigate(-1)
+		}
+	}
+
+	const isNewTab = window.history.length <= 1
 	return (
-		<AppBar sx={{ position: 'sticky', top: 0, backgroundColor: 'primary.dark', boxShadow: 'none', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+		<AppBar
+			sx={{
+				position: 'sticky',
+				top: 0,
+				backgroundColor: 'primary.dark',
+				boxShadow: 'none',
+				zIndex: (theme) => theme.zIndex.drawer + 1,
+			}}
+		>
 			<Toolbar>
 				<IconButton
 					edge="start"
 					color="inherit"
-					onClick={() => navigate(-1)}
-					aria-label="close"
+					onClick={handleButtonClick}
+					aria-label="navigate-back"
 				>
-					<ArrowBack />
+					{isNewTab ? <Close /> : <ArrowBack />}
 				</IconButton>
 				<Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
 					Logging Page
