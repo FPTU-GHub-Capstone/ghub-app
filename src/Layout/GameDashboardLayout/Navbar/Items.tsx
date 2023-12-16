964eb862f3e5d968641d17bf44733c5543241d79
Panh <dangphuonganh200136@gmail.com>
2023-12-16 12:43:58 +0700

    chore: remove unused path in navbar

--- src/Layout/GameDashboardLayout/Navbar/Items.tsx
+++ src/Layout/GameDashboardLayout/Navbar/Items.tsx
@@ -42,19 +42,9 @@ export const NavbarItems: (gameId: string) => NavItem[] = (gameId) => [
 		icon: <Diversity2Icon />,
 	},
 	{
-		title: 'bills',
-		path: `/games/${gameId}/bills`,
-		icon: <ReceiptIcon />,
-	},
-	{
 		title: 'servers',
 		path: `/games/${gameId}/servers`,
 		icon: <StorageIcon />,
-	},
-	{
-		title: 'team',
-		path: `/games/${gameId}/team`,
-		icon: <Diversity2Icon />,
 	},
 	{
 		title: 'players',
